120ca3d0f9f1ee8d7cd84942f1cfeef2dc6a3358
MoonCake-AOV <qingluchenliu123@gmail.com>
2020-08-02 15:07:06 +0200

    upload project

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,13 @@
 #include "helpers.h"
 #include "json.hpp"
 
+#include "spline.h"
+
 // for convenience
 using nlohmann::json;
 using std::string;
 using std::vector;
+
 
 int main() {
   uWS::Hub h;
@@ -50,7 +53,10 @@ int main() {
     map_waypoints_dy.push_back(d_y);
   }
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
+  // initialize with lane 1
+  int lane = 1;
+  double ref_vel = 0;
+  h.onMessage([&lane, &ref_vel, &map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
                &map_waypoints_dx,&map_waypoints_dy]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
@@ -97,6 +103,153 @@ int main() {
            * TODO: define a path made up of (x,y) points that the car will visit
            *   sequentially every .02 seconds
            */
+          // ================prediction start================
+          int prev_size = previous_path_x.size();
+          // if size of previous path data not 0, set car location is at end position of previous path
+          if(prev_size > 0)
+          {
+            car_s = end_path_s;
+          }
+          bool carAtNearfront = false;
+          bool carAtNearleft = false;
+          bool carAtNearright = false;
+          // Use fusion data to predict if surrounding cars near the ego car and may block the lane change
+          for(int i = 0; i < sensor_fusion.size(); i++)
+          {
+            
+            double object_d = sensor_fusion[i][6];
+            int object_lane = (int)(object_d/4);;
+            // check if the car in front is too close
+            double object_vx = sensor_fusion[i][3];
+            double object_vy = sensor_fusion[i][4];
+            double object_speed = sqrt(object_vx*object_vx + object_vy*object_vy);
+            double object_s = sensor_fusion[i][5];
+            object_s += ((double)prev_size * 0.02 * object_speed);
+            if(object_lane == lane){
+              if((object_s > car_s) && (object_s < car_s + 40)){
+                carAtNearfront = true;
+              }
+            } else {
+              if((object_lane == lane - 1) && (object_s>car_s && (object_s - car_s)<40 || object_s<car_s && (car_s - object_s )<40)|| lane == 0)
+                carAtNearleft = true;
+              if((object_lane == lane + 1) && (object_s>car_s && (object_s - car_s)<40 || object_s<car_s && (car_s - object_s )<40)|| lane == 2)
+                carAtNearright = true;
+              }
+          }
+          //================prediction end================
+          std::cout<< "current state: " <<lane << "   " << ref_vel << std::endl;
+          std::cout<< "predictions: " << carAtNearleft << carAtNearright<< carAtNearfront << std::endl;
+          
+          //================behavior planning start================
+          vector<std::pair<int, double>> planned_goalVals;//(lane, velocity)
+          if(carAtNearfront){
+            planned_goalVals.push_back(std::make_pair(lane, ref_vel - 0.224));// 5m/(s^2) acceleration 5 * 0.02m/s=0.224mph
+            if(!carAtNearleft)
+              planned_goalVals.push_back(std::make_pair(std::max(lane - 1, 0), std::min(49.5, ref_vel + 0.224)));//49.5/2.24
+            if(!carAtNearright)
+              planned_goalVals.push_back(std::make_pair(std::min(lane + 1, 2), std::min(49.5, ref_vel + 0.224)));
+          }
+          if(!carAtNearfront){
+            planned_goalVals.push_back(std::make_pair(lane, std::min(49.5, ref_vel + 0.224)));
+          }
+          //================behavior planning end================
+          
+          //================cost function start================
+          double lowest_cost = 1.0;
+          for(int i = 0; i < planned_goalVals.size(); i++){
+            int goalLane = planned_goalVals[i].first;
+            double goalVel = planned_goalVals[i].second;
+            // consider cost of lane change and acceleration
+            double cost_laneChange = std::abs(lane - goalLane)/2; 
+            double cost_Acc = 0.0;
+            if(std::abs((goalVel/2.24 - car_speed/2.24)/0.02) >9.5)
+              cost_Acc = 1.0;
+            else
+              cost_Acc = std::abs((goalVel/2.24 - car_speed/2.24)/0.02)/9.5;
+            //
+            double total_cost=0.5*cost_laneChange+0.5*cost_Acc;
+            if(total_cost <= lowest_cost){
+              lowest_cost = total_cost;
+              lane = goalLane;
+              ref_vel = goalVel;
+            }
+          }
+          //================cost function end================
+          std::cout<< "planned_goalVals size:<<<<<<<<<< " << planned_goalVals.size() << std::endl;
+          std::cout<< "best planned_goalVal choice:  " << lane << "     " << ref_vel << std::endl;
+          
+          //================trajectory generation start================
+          vector<double> points_x;
+          vector<double> points_y;
+          double pos_x = car_x;
+          double pos_y = car_y;
+          double angle = deg2rad(car_yaw);
+
+          // reuse two previous points
+          std::cout<< "prev_size size: " << prev_size << std::endl;
+          if ( prev_size < 2 ) {   
+            points_x.push_back(car_x - cos(car_yaw));
+            points_x.push_back(car_x);    
+            points_y.push_back(car_y - sin(car_yaw));
+            points_y.push_back(car_y);
+          }else{
+            pos_x = previous_path_x[prev_size - 1];
+            pos_y = previous_path_y[prev_size - 1];
+            double pos_x2 = previous_path_x[prev_size - 2];
+            double pos_y2 = previous_path_y[prev_size - 2];
+            angle = atan2(pos_y-pos_y2, pos_x-pos_x2);
+            points_x.push_back(pos_x2);
+            points_x.push_back(pos_x);
+            points_y.push_back(pos_y2);
+            points_y.push_back(pos_y); 
+          }
+          // in Frenet add evenly 30m points ahead of the staring point
+          for(int i = 1; i <4; i++){
+            vector<double> next_point = getXY(car_s + 30*i, (2 + 4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+            points_x.push_back(next_point[0]);
+            points_y.push_back(next_point[1]);
+          }
+          // transform to car coordinates
+          for(int i=0; i <points_x.size(); ++i){
+            double shift_x = points_x[i] - pos_x;
+            double shift_y = points_y[i] - pos_y;
+            points_x[i] = (shift_x*cos(-angle) - shift_y*sin(-angle));
+            points_y[i] = (shift_x*sin(-angle) + shift_y*cos(-angle));
+          }
+          
+          // creat spline
+          tk::spline s;
+          std::cout<< "points_x size: " << points_x.size() << std::endl;
+          for(int index=0;index<points_x.size();index++)
+          {
+            std::cout<< "points_x: " << points_x.at(index) << std::endl;
+          }
+          s.set_points(points_x, points_y);
+          // Using information from the previous path ensures that there is a smooth transition from cycle to cycle
+          for ( int i = 0; i < prev_size; i++ ) {
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
+          }
+          double target_x = 30.0;
+          double target_y = s(target_x);
+          double target_dist = sqrt(target_x*target_x + target_y*target_y);
+          double x_add_on = 0;
+          
+          for( int i = 1; i < 50 - prev_size; i++ ) {
+            double N = target_dist/(0.02*ref_vel/2.24);
+            double x_point = x_add_on + target_x/N;
+            double y_point = s(x_point);
+            x_add_on = x_point;
+            double x_ref = x_point;
+            double y_ref = y_point;
+            x_point = x_ref * cos(angle) - y_ref * sin(angle);
+            y_point = x_ref * sin(angle) + y_ref * cos(angle);
+            x_point += pos_x;
+            y_point += pos_y;
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(y_point);
+          }
+          //================trajectory generation end================
 
 
           msgJson["next_x"] = next_x_vals;
